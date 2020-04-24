1630c4f9f555ec517bce15b6a73123f499590a67
dockermaster <dockermaster@gmail.com>
2020-04-24 12:04:05 -0700

    Added visual validation

--- VisualTesting/UnitTest1.cs
+++ VisualTesting/UnitTest1.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 using Applitools;
 using Applitools.Selenium;
-using Applitools.Utils.Geometry;
 using Applitools.VisualGrid;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using Configuration = Applitools.Selenium.Configuration;
-using ScreenOrientation = Applitools.VisualGrid.ScreenOrientation;
 
 namespace VisualTesting
 {
@@ -18,64 +16,68 @@ namespace VisualTesting
         [TestMethod]
         public void TestMethod2()
         {
-            Eyes eyes;
-            ChromeDriver driver = new ChromeDriver();
-            
-            VisualGridRunner runner = null;
-
             String shouldBreakSiteStr = Environment.GetEnvironmentVariable("INJECT_BUG");
 
             bool shouldBreakSite = false;
 
-            if(shouldBreakSiteStr != null && shouldBreakSiteStr.Length > 0)
+            if (shouldBreakSiteStr != null && shouldBreakSiteStr.Length > 0)
                 shouldBreakSite = bool.Parse(shouldBreakSiteStr);
 
             String testName = "Functional VS Visual";
 
-            runner = new VisualGridRunner(10);
-
-            eyes = new Eyes(runner);
+            VisualGridRunner runner = new VisualGridRunner(10);
+            Eyes eyes = new Eyes(runner);
+            ChromeDriver driver = new ChromeDriver();
 
             Configuration sconf = eyes.GetConfiguration();
 
             sconf.SetAppName(testName);
 
             sconf.SetTestName(testName);
-            
+
             eyes.ApiKey = Environment.GetEnvironmentVariable("APPLITOOLS_API_KEY");
 
             sconf.SetBatch(new BatchInfo(testName));
 
             sconf.AddBrowser(1200, 800, BrowserType.CHROME);
             sconf.AddBrowser(1200, 800, BrowserType.FIREFOX);
+            sconf.AddBrowser(1200, 800, BrowserType.SAFARI);
             sconf.AddBrowser(1200, 800, BrowserType.IE_11);
             sconf.AddBrowser(1200, 800, BrowserType.EDGE);
-            sconf.AddBrowser(1200, 800, BrowserType.SAFARI);
 
-            sconf.AddDeviceEmulation(DeviceName.iPad, ScreenOrientation.Portrait);
-            sconf.AddDeviceEmulation(DeviceName.iPad_Pro, ScreenOrientation.Portrait);
-            sconf.AddDeviceEmulation(DeviceName.iPhone_6_7_8_Plus, ScreenOrientation.Portrait);
-            sconf.AddDeviceEmulation(DeviceName.iPhone_X, ScreenOrientation.Portrait);
-            sconf.AddDeviceEmulation(DeviceName.Galaxy_Note_3, ScreenOrientation.Portrait);
-            sconf.AddDeviceEmulation(DeviceName.Nexus_10, ScreenOrientation.Portrait);
 
-            eyes.SetLogHandler(new StdoutLogHandler());
-            
+            //sconf.AddDeviceEmulation(DeviceName.iPad, ScreenOrientation.Portrait);
+            //sconf.AddDeviceEmulation(DeviceName.iPad_Pro, ScreenOrientation.Portrait);
+            //sconf.AddDeviceEmulation(DeviceName.iPhone_6_7_8_Plus, ScreenOrientation.Portrait);
+            //sconf.AddDeviceEmulation(DeviceName.iPhone_X, ScreenOrientation.Portrait);
+            //sconf.AddDeviceEmulation(DeviceName.Galaxy_Note_3, ScreenOrientation.Portrait);
+            //sconf.AddDeviceEmulation(DeviceName.Nexus_10, ScreenOrientation.Portrait);
+
+            eyes.SetLogHandler(new FileLogHandler(@"C:\Users\user\Desktop\appli.log", false , true));
+
             sconf.SetViewportSize(1000, 600);
-            
+
             eyes.SetConfiguration(sconf);
 
+            Console.WriteLine("Open conection to Eyes");
             eyes.Open(driver);
-  
+
             driver.Url = "https://github.com/login";
 
-            eyes.CheckWindow("Login Page");
+            Console.WriteLine("Visual Assertion #1");
+            eyes.Check(Target.Window().Fully().WithName("Login page"));
 
+            Console.WriteLine("Click Login");
             driver.FindElement(By.CssSelector("#login > form > div.auth-form-body.mt-3 > input.btn.btn-primary.btn-block"))
                     .Click();
 
             if (shouldBreakSite)
+            {
+                Console.WriteLine("Breaking Site");
                 BreakSite(driver);
+            }
+
+            Console.WriteLine("Begin Functional Assertions");
 
             // validate sign in button
             String buttonText = driver
@@ -100,20 +102,19 @@ namespace VisualTesting
 
             Assert.IsTrue(passwordTextbox.Contains("Password"), "wrong label");
 
-            eyes.CheckWindow("Error Message");
+            Console.WriteLine("Completed Functional Assertions");
 
-            try
-            {
+            Console.WriteLine("Visual Assertion #2");
+            eyes.Check(Target.Window().Fully().WithName("Error Message"));
 
-                eyes.CloseAsync();
+            Console.WriteLine("Close connection to Eyes");
+            eyes.CloseAsync();
 
-                Console.WriteLine(runner.GetAllTestResults(false));
+            driver.Quit();
 
-            }
-            finally
-            {
-                driver.Quit();
-            }
+            System.Diagnostics.Debug.WriteLine("Waiting for visual test to complete");
+            TestResultsSummary results = runner.GetAllTestResults();
+            System.Diagnostics.Debug.WriteLine(results);
         }
 
         private static void BreakSite(ChromeDriver driver)
