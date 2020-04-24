ee936f25f17044707b438e727b0a756f0fb22c25
dockermaster <dockermaster@gmail.com>
2020-04-23 23:42:01 -0700

    Add project files.

---
+++ VisualTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("VisualTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VisualTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f33b869b-e018-4e1d-b8d9-16c05c7c3811")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ VisualTesting/UnitTest1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using Applitools;
+using Applitools.Selenium;
+using Applitools.Utils.Geometry;
+using Applitools.VisualGrid;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using Configuration = Applitools.Selenium.Configuration;
+using ScreenOrientation = Applitools.VisualGrid.ScreenOrientation;
+
+namespace VisualTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Eyes eyes;
+            InternetExplorerDriver driver = new InternetExplorerDriver();
+            
+            VisualGridRunner runner = null;
+
+            String shouldBreakSiteStr = Environment.GetEnvironmentVariable("INJECT_BUG");
+
+            bool shouldBreakSite = false;
+
+            if(shouldBreakSiteStr != null && shouldBreakSiteStr.Length > 0)
+                shouldBreakSite = bool.Parse(shouldBreakSiteStr);
+
+            String testName = "Functional VS Visual";
+
+            runner = new VisualGridRunner(10);
+
+            eyes = new Eyes(runner);
+
+            driver = new ChromeDriver();
+
+            Configuration sconf = eyes.GetConfiguration();
+
+            sconf.SetAppName(testName);
+
+            sconf.SetTestName(testName);
+
+            sconf.SetBatch(new BatchInfo(testName));
+
+            sconf.AddBrowser(1200, 800, BrowserType.CHROME);
+            sconf.AddBrowser(1200, 800, BrowserType.FIREFOX);
+            sconf.AddBrowser(1200, 800, BrowserType.IE_11);
+            sconf.AddBrowser(1200, 800, BrowserType.EDGE);
+            //sconf.AddBrowser(1200, 800, BrowserType.SAFARI);
+
+            //sconf.AddDeviceEmulation(DeviceName.iPad, ScreenOrientation.Portrait);
+            //sconf.AddDeviceEmulation(DeviceName.iPad_Pro, ScreenOrientation.Portrait);
+            //sconf.AddDeviceEmulation(DeviceName.iPhone_6_7_8_Plus, ScreenOrientation.Portrait);
+            //sconf.AddDeviceEmulation(DeviceName.iPhone_X, ScreenOrientation.Portrait);
+            //sconf.AddDeviceEmulation(DeviceName.Galaxy_Note_3, ScreenOrientation.Portrait);
+            //sconf.AddDeviceEmulation(DeviceName.Nexus_10, ScreenOrientation.Portrait);
+
+            eyes.SetLogHandler(new StdoutLogHandler());
+            eyes.SetConfiguration(sconf);
+
+            eyes.Open(driver);
+  
+            driver.Url = "https://github.com/login";
+
+            eyes.CheckWindow("Login Page");
+
+            driver.FindElement(By.CssSelector("#login > form > div.auth-form-body.mt-3 > input.btn.btn-primary.btn-block"))
+                    .Click();
+
+            if (shouldBreakSite)
+                BreakSite(driver);
+
+            // validate sign in button
+            String buttonText = driver
+                    .FindElement(
+                            By.CssSelector("#login > form > div.auth-form-body.mt-3 > input.btn.btn-primary.btn-block"))
+                    .GetAttribute("value");
+
+            Assert.IsTrue(buttonText.CompareTo("Sign in") == 0, "wrong button");
+
+            // validate error message
+            String errorMessage = driver.FindElement(By.CssSelector("#js-flash-container > div > div")).Text;
+
+            Assert.IsTrue(errorMessage.Contains("Incorrect username or password."), "wrong label");
+
+            String usernameTextbox = driver
+                    .FindElement(By.CssSelector("#login > form > div.auth-form-body.mt-3 > label:nth-child(1)")).Text;
+
+            Assert.IsTrue(usernameTextbox.Contains("Username or email address"), "wrong label");
+
+            String passwordTextbox = driver
+                    .FindElement(By.CssSelector("#login > form > div.auth-form-body.mt-3 > label:nth-child(3)")).Text;
+
+            Assert.IsTrue(passwordTextbox.Contains("Password"), "wrong label");
+
+            eyes.CheckWindow("Error Message");
+
+            try
+            {
+
+                eyes.CloseAsync();
+
+                Console.WriteLine(runner.GetAllTestResults(false));
+
+            }
+            finally
+            {
+                driver.Quit();
+            }
+        }
+
+        private static void BreakSite(ChromeDriver driver)
+        {
+            
+            driver.ExecuteScript("arguments[0].setAttribute('style','color: red')",
+                    driver.FindElement(By.CssSelector("#login > form > div.auth-form-body.mt-3 > label:nth-child(1)")));
+            driver.ExecuteScript("arguments[0].setAttribute('style','color: pink')",
+                    driver.FindElement(By.CssSelector("#login > form > div.auth-form-body.mt-3 > label:nth-child(3)")));
+            driver.ExecuteScript("arguments[0].setAttribute('style','fill: green')",
+                    driver.FindElement(By.CssSelector(
+                            "body > div.position-relative.js-header-wrapper > div.header.header-logged-out.width-full.pt-5.pb-4 > div > a > svg")));
+            driver.ExecuteScript("arguments[0].setAttribute('style','font-size: 8px')",
+                    driver.FindElement(By.CssSelector("#login > p")));
+
+        }
+
+    }
+}
